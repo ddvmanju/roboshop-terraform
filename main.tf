f43ba3f40c1bb27aadd6f1ae4a2b1cf695f68fb4
Master CVV <ddv.manju@gmail.com>
2025-09-02 12:34:32 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ module "components" {
   for_each  = var.components
   source    = "./modules/vm"
   component = each.value["name"]
-  vm_size   = each.value["vm_size"]
+  vm_size   = each.value["vm_size"] # Use dot notation
   env       = var.env
 }
-
