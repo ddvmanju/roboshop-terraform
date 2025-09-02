881c54be1f02adb9ea1869852db7d7d63094dfe6
Master CVV <ddv.manju@gmail.com>
2025-09-02 12:52:50 +0530

    roboshop terraform

--- main.tf
+++ main.tf
@@ -4,5 +4,4 @@ module "components" {
   component = each.value["name"]
   vm_size   = each.value["vm_size"] # Use dot notation
   env       = var.env
-  subnet_id = data.azurerm_subnet.main.id
-}
+  }
